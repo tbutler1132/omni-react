ab11bfa378a2d2327d67b39db8b691b843edd9bd
Tim Butler <timbutler@Tims-MacBook-Pro.local>
2023-09-24 11:38:50 -0400

    .

--- src/features/posts/postsAPI.js
+++ src/features/posts/postsAPI.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const postsApi = createApi({
     reducerPath: 'postsApi',
-    baseQuery: fetchBaseQuery({ baseUrl: process.env.NODE_ENV === 'production' ?  'http://localhost:3001/' : 'http://localhost:3001/'}),
+    baseQuery: fetchBaseQuery({ baseUrl: process.env.NODE_ENV === 'production' ?  'https://omni-blog-server-401a3abbae8e.herokuapp.com/' : 'http://localhost:3001/'}),
     endpoints: (builder) => ({
         getPosts: builder.query({
             query: () => `posts`
